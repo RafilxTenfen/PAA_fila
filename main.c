e1e124cb0171869dc669cdf4397552c081eca88d
Rafilx Tenfen <rafaeltenfen.rt@gmail.com>
2021-04-07 11:29:24 -0300

    test fila

--- main.c
+++ main.c
@@ -10,4 +10,27 @@ int main(int argc, char** argv) {
   int result = enfileirar(pFila, &enf1);
   printf("\nenfileirar result: %d -> %d", result, enf1);
 
+  int enf2 = 8;
+  int result2 = enfileirar(pFila, &enf2);
+  printf("\nenfileirar result2: %d -> %d", result2, enf2);
+
+  int enf3 = 15;
+  int result3 = enfileirar(pFila, &enf3);
+  printf("\nenfileirar result3: %d -> %d", result3, enf3);
+
+  int* desenf1 = desenfileira(pFila);
+  printf("\ndesenfileirar -> %d", *desenf1);
+
+  int* desenf2 = desenfileira(pFila);
+  printf("\ndesenfileirar -> %d", *desenf2);
+
+  int* desenf3 = desenfileira(pFila);
+  printf("\ndesenfileirar -> %d", *desenf3);
+
+  int* desenf4 = desenfileira(pFila);
+  if (desenf4 == NULL) {
+    printf("\ndesenfileirar IS NULL");
+  }
+
+
 }
